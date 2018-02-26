77d75702317df51272d8c7381d133b4bbbf90111
Anuj Verma <anujver23@gmail.com>
2018-02-26 15:03:54 +0530

    Adds constraint on external_id and source Adds extra fields

--- db/migrations/00001_create_table_products.sql
+++ db/migrations/00001_create_table_products.sql
@@ -1,12 +1,20 @@
 -- +goose Up
 CREATE TABLE profiles (
     id serial primary key,
+    name varchar(50),
     username varchar(50),
     email varchar(50),
     image_url text,
     phone varchar(15),
     source varchar(50),
-    url text
+    url text,
+    location varchar(20),
+    description text,
+    external_id bigint,
+    created_at timestamp,
+    updated_at timestamp,
+    birthdate timestamp,
+    CONSTRAINT external_id_source UNIQUE(external_id, source)
 );
 -- +goose Down
 DROP TABLE profiles;
